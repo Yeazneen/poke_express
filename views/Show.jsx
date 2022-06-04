43326ae6b64f7b78a48708cc489c238e3c874aed
Farah Yeazneen <farahyeazneen@Farahs-Air.home>
2022-06-04 01:10:57 -0400

    Fixed errors

--- views/Show.jsx
+++ views/Show.jsx
@@ -7,17 +7,24 @@ const myStyle = {
     textAlign: 'center',
 };
 
+const imgStyle = {
+    justifyContent:'center',
+    alignItems: 'center',
+}
+
 class Show extends React.Component{
     render(){
         const pokemon = this.props.pokemon
         return (
             <div>
                 <h1 style={myStyle}>Gotta Catch 'Em All</h1>
+                <div>
                 <nav>
                     <a href="/pokemon">Back</a>
                 </nav> 
                 <h2>{pokemon.name}</h2>
                 <image style={imgStyle} src={`${pokemon.img}.jpg`} alt={this.props.alt}></image>  
+                </div>
             </div>
         )
     }
